1d2b6af5514f52238c42d621a4064a347a7e29cb
casaxmore <casaxmore@gmail.com>
2023-02-20 22:44:56 +0100

    busqueda y vista pelicula

--- src/app/movie/pages/pelicula/pelicula.component.ts
+++ src/app/movie/pages/pelicula/pelicula.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-pelicula',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./pelicula.component.css']
 })
 export class PeliculaComponent {
+
+  pelicula: any;
+  regresarA: string = '';
+  busqueda: string = '';
+
+  constructor(public movieService: MovieService,
+              public route: ActivatedRoute) {
+
+    this.route.params.subscribe( parametros => {
+    console.log(parametros);
+    this.regresarA = parametros['pag'];
+
+    if (parametros['busqueda']) {
+      this.busqueda = parametros['busqueda'];
+    }
+
+    this.movieService.getPelicula( parametros['id'] )
+        .subscribe( peli => {
+          this.pelicula = peli;
+          console.log(this.pelicula);
+        });
+    });
+  }
 
 }
